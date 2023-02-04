b0eb3f77827def99e6861e109290b848112d9098
ArthurTran12 <athur@coinhe.io>
2023-02-04 09:16:22 +0700

    install prettier, lint-stated, husky

--- src/index.js
+++ src/index.js
@@ -1,34 +1,36 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const { engine } = require("express-handlebars");
-const morgan = require("morgan");
-const path = require("path");
+const { engine } = require('express-handlebars');
+const morgan = require('morgan');
+const path = require('path');
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 
 // HTTP Logger
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 // Teamplate handle engine
 app.engine(
-  "hbs",
-  engine({
-    extname: ".hbs",
-  })
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
 );
-app.set("view engine", "hbs");
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
-app.set("views", path.join(__dirname, "resources/views"));
-
-app.get("/", (req, res) => {
-  return res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  return res.render("news");
-});
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port http://localhost:${port}`);
 });
