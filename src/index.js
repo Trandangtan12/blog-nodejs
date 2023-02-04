ebb216357b1132235f62d1f12661324b97192e47
ArthurTran12 <athur@coinhe.io>
2023-02-04 09:17:34 +0700

    install prettier, lint-stated, husky

--- src/index.js
+++ src/index.js
@@ -31,6 +31,6 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 route(app);
 
-app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`);
+                   app.listen(port, () => {
+                            console.log(`Example app listening on port http://localhost:${port}`);
 });
